dc4a5f526fa32df833d46574ac3810184da749b3
Peraboto <tobiasperabo@premium-cases.de>
2026-05-30 16:59:35 +0200

    Update template directory path in main.py

--- ha_maintenance_center/app/main.py
+++ ha_maintenance_center/app/main.py
@@ -12,7 +12,7 @@ from containers import container_stats
 
 app = FastAPI(title="HA Maintenance Center")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="/app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
